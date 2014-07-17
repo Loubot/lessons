b7fd85bffd66e38d20bfa4a0baa68e08d5ad163d
Louis <louisangelini@gmail.com>
2014-07-17 14:00:55 +0100

    Add s3 storage to carrierwave

--- Gemfile
+++ Gemfile
@@ -59,6 +59,8 @@ gem 'date_validator'
 
 gem 'rmagick'
 
+gem "fog"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+ 
+	config.fog_credentials = {
+	    :provider               => 'AWS',                        # required
+	    :aws_access_key_id      => 'AKIAIF6BT2RRR3QFAK6Q',                        # required
+	    :aws_secret_access_key  => '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',                        # required
+	    :region                 => 'eu-west-1',
+	    :path_style => true
+	  }
+	  config.fog_directory  = 'lessons.photos'                     # required
+	  config.fog_public     = false                                   # optional, defaults to true
+	  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  #
+end
