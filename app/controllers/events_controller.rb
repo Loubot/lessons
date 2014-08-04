c6ea05b4a9dcaf139391ef001c9e4886bc18f8a1
Louis <louisangelini@gmail.com>
2014-08-04 14:51:05 +0100

    Add code to do block bookings

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,32 +1,25 @@
 class EventsController < ApplicationController
 	def create
-		if params['Multiple']
-			flash[:success] = params['booking_length']
-		end
-		
 		if params[:date].blank? 
 			flash[:danger] = "Date can't be blank"
 			redirect_to :back and return
 		end
-		# save_params = format_time(params)
-		# @event = Event.new(save_params)
-		ids = []
-		2.times do 
-			e = Event.new(create_params(Time.now))
-			if e.save
-				flash[:success] = 'cool'
-				ids << e.id
-			else
-				flash[:danger] = 'nope'
-				puts "#{e.errors.full_messages}"
-				ids.each do |e|
-					event = Event.find(e.to_i)
-					event.destroy
-				end
-				redirect_to :back and return
-				
-			end
+
+		if params['Multiple']
+			doMultipleBookings(params)
+			redirect_to :back and return
 		end
+		
+		@event = Event.new(format_time(params))
+		if @event.save
+			flash[:success] = "Lesson created successfully"
+			redirect_to :back
+		else
+			flash[:danger] = "Couldn't save lesson #{@event.errors.full_messages}"
+			redirect_to :back
+		end
+		
+	end
 
 
 	def edit
@@ -55,5 +48,34 @@ class EventsController < ApplicationController
 		starttime = Time.zone.parse("#{date} #{params[:event]['start_time(5i)']}")
 		endtime = Time.zone.parse("#{date} #{params[:event]['end_time(5i)']}")
 		@event_params = { time_off: params[:event][:time_off], start_time: starttime, end_time: endtime, status: 'active', teacher_id: params[:event][:teacher_id]}
+	end
+
+	def doMultipleBookings(params)
+		ids = []
+		continue = true
+		date = Date.parse(params[:date])
+		startTime = Time.parse("#{date}, #{params[:event]['start_time(5i)']}")
+		endTime = Time.parse("#{date}, #{params[:event]['end_time(5i)']}")
+		puts startTime + 7.days
+		puts endTime + 7.days
+		weeks = params[:booking_length].to_i - 1
+		for i in 0..weeks
+			newStart = startTime + (i*7.days)
+			newEnd = endTime + (i*7.days)
+			e = Event.new(start_time: newStart, end_time: newEnd, status: 'active',
+									 teacher_id: current_teacher.id)
+			if e.save
+				ids << e.id
+			else 
+				flash[:danger] = "There was a booking conflict #{e.errors.full_messages}"
+				ids.each do |id|
+					Event.find(id).destroy					
+				end
+				continue = false
+				return
+			end
+			return unless continue
+		end
+
 	end
 end
