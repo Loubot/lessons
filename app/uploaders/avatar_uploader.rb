43934e94fe9313e5e0f763273a7bc6dd59dd0653
Louis Angelini <lllouis@yahoo.com>
2014-08-14 23:46:08 +0100

    Configure app for windows

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,12 +3,12 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  #include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
-  storage :fog
+  storage :file
+  #storage :fog
 
   def root
     Rails.root.join 'public/'
@@ -37,9 +37,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  version :thumb do
-    process :resize_to_fill => [200, 200]
-  end
+  # version :thumb do
+  #   process :resize_to_fill => [200, 200]
+  # end
 
   #Create different versions of your uploaded files:
   # version :thumb do
