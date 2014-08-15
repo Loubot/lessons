dfc20281cb83ec000240a2dbf118377715b1ac27
Louis Angelini <lllouis@yahoo.com>
2014-08-15 06:14:54 +0100

    Add silhouette helper to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+	def silhouette_helper(teacher)
+		teacher.profile == nil ? image_tag('silhouette.jpg') : image_tag(Photo.find(teacher.profile).avatar.url,size: '100x100', class: 'pull-left results_photos') 
+	end
 end
