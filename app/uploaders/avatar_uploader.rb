d4a6b9ec7b1cc7cbdc4141326e213a127a9b5411
Louis <louisangelini@gmail.com>
2014-08-19 17:40:07 +0100

    Config app for heroku

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,12 +3,12 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  #include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  #storage :fog
+#storage :file
+  storage :fog
 
   def root
     Rails.root.join 'public/'
@@ -42,9 +42,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   #Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
