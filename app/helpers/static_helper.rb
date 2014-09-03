ab40b19d7ca11e8741dd76a4e1d30ae5154ac7e0
Louis <louisangelini@gmail.com>
2014-09-03 14:37:15 +0100

    Add else branch to handle distance selection

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -2,8 +2,8 @@ module StaticHelper
 
   def get_search_results(params)
     if !params[:search_position].empty? && !params[:search_subjects].empty?
-      @subject = params[:search_subjects] == '' ? [] : Subject.where('name ILIKE ?', "%#{params[:search_subjects]}%").first
-
+      @subject = get_subject(params[:search_subjects])
+      puts "%%%%%%%%%%%%#{@subject.name}"
       if params[:sort_by] == 'Rate: lowest first'
         
         @teachers = defined?(@subject.teachers) ? @subject.teachers.near(params[:search_position], 50).reorder('rate ASC') : []
@@ -11,6 +11,8 @@ module StaticHelper
       elsif params[:sort_by] == "Rate: highest first"
         @teachers = defined?(@subject.teachers) ? @subject.teachers.near(params[:search_position], 50).reorder('rate DESC')
            : []
+      else
+        @teachers = defined?(@subject.teachers) ? @subject.teachers.near(params[:search_position], 50) : []
       end
 
     elsif !params[:search_position].empty? && params[:search_subjects].empty?
@@ -37,6 +39,11 @@ module StaticHelper
       []
         
     end
+  end
+
+  def get_subject(subject)
+    @subject = subject == '' ? [] : Subject.where('name ILIKE ?', "%#{subject}%").first
+
   end
 end
 
