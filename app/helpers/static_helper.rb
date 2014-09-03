01676f76366a8fd09076ddf71be36aa381ce6eab
Louis <louisangelini@gmail.com>
2014-09-03 14:41:16 +0100

    Tidy up static helper

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -3,11 +3,9 @@ module StaticHelper
   def get_search_results(params)
     if !params[:search_position].empty? && !params[:search_subjects].empty?
       @subject = get_subject(params[:search_subjects])
-      puts "%%%%%%%%%%%%#{@subject.name}"
-      if params[:sort_by] == 'Rate: lowest first'
-        
+      
+      if params[:sort_by] == 'Rate: lowest first'        
         @teachers = defined?(@subject.teachers) ? @subject.teachers.near(params[:search_position], 50).reorder('rate ASC') : []
-
       elsif params[:sort_by] == "Rate: highest first"
         @teachers = defined?(@subject.teachers) ? @subject.teachers.near(params[:search_position], 50).reorder('rate DESC')
            : []
@@ -26,7 +24,8 @@ module StaticHelper
       end          
 
     elsif params[:search_position].empty? && !params[:search_subjects].empty?
-      @subject = params[:search_subjects] == '' ? [] : Subject.where('name ILIKE ?', "%#{params[:search_subjects]}%").first
+      @subject = get_subject(params[:search_subjects])
+
       if params[:sort_by] == 'Rate: lowest first'
         @teachers = defined?(@subject.teachers) ? @subject.teachers.order('rate ASC') : []
       elsif params[:sort_by] == 'Rate: highest first'
@@ -43,7 +42,6 @@ module StaticHelper
 
   def get_subject(subject)
     @subject = subject == '' ? [] : Subject.where('name ILIKE ?', "%#{subject}%").first
-
   end
 end
 
