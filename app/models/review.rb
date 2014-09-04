acb9ee306f886d94aff8e893cac4d9277564cae9
Louis <louisangelini@gmail.com>
2014-09-04 23:04:05 +0100

    Change review validation inclusion

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,8 +11,9 @@
 #
 
 class Review < ActiveRecord::Base
-  has_and_belongs_to_many :teachers
-  has_and_belongs_to_many :users
+  belongs_to :teachers
+  belongs_to :users
 
+  validates :rating, inclusion: 1..5
   validates :teacher_id, :rating, :user_id, presence: true
 end
