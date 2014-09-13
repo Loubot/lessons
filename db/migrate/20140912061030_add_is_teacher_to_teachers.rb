1f21e6075caee891e18381cb1d2c17cca16bcd24
Louis <louisangelini@gmail.com>
2014-09-13 21:37:16 +0100

    Change migration to set is_teacher to false

--- db/migrate/20140912061030_add_is_teacher_to_teachers.rb
+++ db/migrate/20140912061030_add_is_teacher_to_teachers.rb
@@ -1,5 +1,5 @@
 class AddIsTeacherToTeachers < ActiveRecord::Migration
   def change
-    add_column :teachers, :is_teacher, :boolean
+    add_column :teachers, :is_teacher, :boolean, null: false, default: false
   end
 end
