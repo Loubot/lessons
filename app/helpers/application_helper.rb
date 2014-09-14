1b6ee8b74c82000fad0deaee7c690e3d301b5920
Louis <louisangelini@gmail.com>
2014-09-14 22:09:29 +0100

    Add begin rescue to silhouette helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
 	def silhouette_helper(teacher)
-		teacher.profile == nil ? image_tag('silhouette.jpg', size: '125x125') : image_tag(Photo.find(teacher.profile).avatar.url, size: '125x125', class: 'pull-left results_photos front') 
+    begin
+		  teacher.profile == nil ? image_tag('silhouette.jpg', size: '125x125') : image_tag(Photo.find(teacher.profile).avatar.url, size: '125x125', class: 'pull-left results_photos front') 
+    rescue
+      image_tag('silhouette.jpg', size: '125x125')
+    end
 	end
 
   def resource_name
