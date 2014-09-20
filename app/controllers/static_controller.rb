62a05e87350f7a2d71bf1816538027673e30c38b
Louis <louisangelini@gmail.com>
2014-09-20 14:58:57 +0100

    Action to check validity of paypal payment

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,8 +10,38 @@ class StaticController < ApplicationController
 		
 	end
 
-	def welcome
-		create_paypal(params)
+	def welcome		
+		create_paypal(params) if params[:paypal].present?
+	end
+
+	def store_paypal
+		puts "???????????????? #{params}"
+		flash[:success] = params
+		redirect_to root_url
+	end
+
+	def paypal_return
+		
+		require "pp-adaptive"
+
+		client = AdaptivePayments::Client.new(
+		  :user_id       => "lllouis_api1.yahoo.com",
+		  :password      => "MRXUGXEXHYX7JGHH",
+		  :signature     => "AFcWxV21C7fd0v3bYYYRCpSSRl31Akm0pm37C5ZCuhi7YDnTxAVFtuug",
+		  :app_id        => "APP-80W284485P519543T",
+		  :sandbox       => true
+		)
+
+		client.execute(:PaymentDetails, :pay_key => "AP-4TS489127N381100H") do |response|
+		  if response.success?
+		    puts "Payment status: #{response.inspect}"
+		    flash[:success] = "Payment status: #{response.inspect}"
+		  else
+		    puts "#{response.ack_code}: #{response.error_message}"
+		  end
+		end
+
+		redirect_to root_url
 	end
 
 	def learn
@@ -55,7 +85,7 @@ class StaticController < ApplicationController
 
 		def create_paypal(params)
 			require "pp-adaptive"
-
+			puts "444444444444 #{params[:receiver_amount].to_f}"
 			client = AdaptivePayments::Client.new(
 			  :user_id       => "lllouis_api1.yahoo.com",
 			  :password      => "MRXUGXEXHYX7JGHH",
@@ -68,9 +98,10 @@ class StaticController < ApplicationController
 			  :action_type     => "PAY",
 			  :receiver_email  => "louisangelini@gmail.com",
 			  :receiver_amount => params[:receiver_amount],
-			  :currency_code   => "EUR",
+			  :currency_code   => "GBP",
 			  :cancel_url      => "https://learn-your-lesson.herokuapp.com",
-			  :return_url      => "https://ylearn-your-lesson.herokuapp.com"
+			  :return_url      => "http://localhost:3000/paypal-return",
+			  :notify_URL			 => 'http://69bbab3a.ngrok.com/store-paypal'
 			) do |response|
 
 			  if response.success?
@@ -96,3 +127,4 @@ end
 #https://github.com/paypal/adaptivepayments-sdk-ruby
 
 #curl -s --insecure -H "X-PAYPAL-SECURITY-USERID: lllouis_api1.yahoo.com" -H "X-PAYPAL-SECURITY-PASSWORD: MRXUGXEXHYX7JGHH" -H "X-PAYPAL-SECURITY-SIGNATURE: AFcWxV21C7fd0v3bYYYRCpSSRl31Akm0pm37C5ZCuhi7YDnTxAVFtuug" -H "X-PAYPAL-REQUEST-DATA-FORMAT: JSON" -H "X-PAYPAL-RESPONSE-DATA-FORMAT: JSON" -H "X-PAYPAL-APPLICATION-ID: APP-80W284485P519543T" https://svcs.sandbox.paypal.com/AdaptivePayments/Pay -d "{\"actionType\":\"PAY\", \"currencyCode\":\"USD\", \"receiverList\":{\"receiver\":[{\"amount\":\"1.00\",\"email\":\"rec1_1312486368_biz@gmail.com\"}]}, \"returnUrl\":\"http://www.example.com/success.html\", \"cancelUrl\":\"http://www.example.com/failure.html\", \"requestEnvelope\":{\"errorLanguage\":\"en_US\", \"detailLevel\":\"ReturnAll\"}}"
+#dlGkcgUigxqJ-mMcnj6jgbbJv9uGNdyhcAV4UPwo8ev4EEhT-KEM6qPGkpG
