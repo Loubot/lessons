dea267b3bf8589b3616dc7564dc93609cff20503
Louis Angelini <lllouis@yahoo.com>
2014-10-22 11:27:32 +0100

    Config app for windows and tidy up layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-    if request.referer == sign_in_url
-      super
+
+    if URI(request.referer).path == teach_path || URI(request.referer).path == learn_path
+      root_path 
     else
       stored_location_for(resource) || request.referer || root_path
-    end
+    end    
   end
     
   def after_sign_up_path_for(resource)
