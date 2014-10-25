841e3899b3b0b32f279c2bc4854dfaa5521fe25e
Louis <louisangelini@gmail.com>
2014-10-25 15:24:53 +0100

    Change dnv variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS', # required
-    :aws_access_key_id => ENV['aws_key'], # required
-    :aws_secret_access_key => ENV['aws_secret_key'], # required
+    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'], # required
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], # required
     :region => 'eu-west-1',
     :path_style => true
   }
