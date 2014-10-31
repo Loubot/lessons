4e1676d4b08391a6dcc7938ee15adc008161bcb7
Louis Angelini <lllouis@yahoo.com>
2014-10-31 08:53:13 +0000

    Alter background images

--- db/schema.rb
+++ db/schema.rb
@@ -178,24 +178,4 @@ ActiveRecord::Schema.define(version: 20141029144614) do
 
   add_index "user_carts", ["tracking_id"], name: "index_user_carts_on_tracking_id", unique: true
 
-  create_table "users", force: true do |t|
-    t.text     "first_name"
-    t.text     "last_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
 end
