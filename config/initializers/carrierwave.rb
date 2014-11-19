9ce9673ecb2d6d7b45cd38a6e9bbf1f066178342
unknown <angell@IRMFSCANST180.corp.emc.com>
2014-11-19 15:34:16 +0000

    Remove logo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider => 'AWS', # required
-    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'], # required
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], # required
-    :region => 'eu-west-1',
-    :path_style => true
-  }
-  config.fog_directory = 'lessons-photos' # required
-  config.fog_public = false # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} #
+  # config.fog_credentials = {
+  #   :provider => 'AWS', # required
+  #   :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'], # required
+  #   :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], # required
+  #   :region => 'eu-west-1',
+  #   :path_style => true
+  # }
+  # config.fog_directory = 'lessons-photos' # required
+  # config.fog_public = false # optional, defaults to true
+  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} #
 end
