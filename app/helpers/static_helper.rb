326fdd8adb52b1d31ba3d2b81d23d402b8dd4c79
Louis <louisangelini@gmail.com>
2015-01-18 14:15:27 +0000

    Tidy up search function

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -9,34 +9,38 @@ end
 
 
 def get_search_results(params, subject) #return list of valid teachers ordered by params
-  ids = Location.near('cork', 10).select('id').map(&:teacher_id)
-  @teachers = subject.first.teachers.check_if_valid.includes(:prices, :reviews, :subjects).where(id: ids).paginate(page: params[:page])
+  puts "postion #{params[:search_position]}"
   
-    if !params[:search_position].empty? && !params[:search_subjects].empty?
-      
+  
+    if !params[:search_position].empty? && !params[:search_subjects].empty? #subject and location
+      ids = Location.near(params[:search_position], 10).select('id').map(&:teacher_id)
+      @teachers = subject.first.teachers.check_if_valid.includes(:prices, :reviews, :subjects).where(id: ids).paginate(page: params[:page])
       
       if params[:sort_by] == 'Rate: lowest first'   
-        @teachers = defined?(subject.teachers) ? subject.teachers.check_if_valid.includes(:prices, :reviews).near(params[:search_position], 50).order("prices.home_price ASC") : []
+        @teachers.reorder('prices.price ASC')
       elsif params[:sort_by] == "Rate: highest first"
-        @teachers = defined?(subject.teachers) ? subject.teachers.check_if_valid.includes(:prices, :reviews).near(params[:search_position], 50).order("prices.home_price DESC")
-           : []
+        @teachers.reorder('prices.price DESC')
       else
-        @teachers = defined?(subject.teachers) ? subject.teachers.check_if_valid.includes(:prices, :reviews).near(params[:search_position], 50) : []
+        @teachers
       end
 
-    elsif !params[:search_position].empty? && params[:search_subjects].empty?
-      
+    elsif !params[:search_position].empty? && params[:search_subjects].empty? #location but not subject
+      ids = Location.near(params[:search_position], 10).select('id').map(&:teacher_id)
+      @teachers = subject.first.teachers.check_if_valid.includes(:prices, :reviews, :subjects).where(id: ids).paginate(page: params[:page])
+
       if params[:sort_by] == 'Rate: lowest first'
-        Teacher.check_if_valid.includes(:prices, :reviews).near(params[:search_position], 50).order("prices.home_price ASC")
+        @teachers.reorder('prices.price ASC')
       elsif params[:sort_by] == 'Rate: highest first'
-        Teacher.check_if_valid.includes(:prices, :reviews).near(params[:search_position], 50).order("prices.home_price DESC")
+        @teachers.reorder('prices.price DESC')
       else
-        Teacher.check_if_valid.includes(:prices, :reviews).near(params[:search_position], 50)
+        @teachers
       end          
 
-    elsif params[:search_position].empty? && !params[:search_subjects].empty?
-      
+    elsif params[:search_position].empty? && !params[:search_subjects].empty? #no location and subject
+      # ids = Location.near(params[:search_position], 10).select('id').map(&:teacher_id)
+      @teachers = subject.first.teachers.check_if_valid.includes(:prices, :reviews, :subjects).paginate(page: params[:page])
 
+      puts 'yaya'
       if params[:sort_by] == 'Rate: lowest first'
         
         @teachers.reorder('prices.price ASC')
@@ -50,7 +54,7 @@ def get_search_results(params, subject) #return list of valid teachers ordered b
         @teachers 
       end
 
-    elsif params[:search_position].empty? && params[:search_subjects].empty?
+    elsif params[:search_position].empty? && params[:search_subjects].empty? #no location or subject
       []
     else
       []
