83e8a283305f2d5b7c1b55c00b264dbea4fe6b22
Louis Angelini <lllouis@yahoo.com>
2015-01-24 19:01:42 +0000

    change font-size

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -90,9 +90,9 @@ module TeachersHelper
 
 	def flash_class_name(name)
 		case name
-		when 'notice' then 'success'
-		when 'alert' 	then 'danger'
-		else name
+			when 'notice' then 'success'
+			when 'alert' 	then 'danger'
+			else name
 		end
   end
 
