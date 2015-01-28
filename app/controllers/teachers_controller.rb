cf770a74457a34ec58450deeec8b9db482c067f0
Louis <louisangelini@gmail.com>
2015-01-28 14:42:23 +0000

    Remove profile pic from fotorama and fix padding on scheduler

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -25,7 +25,7 @@ class TeachersController < ApplicationController
 		gon.profile_pic_url = @teacher.photos.find(@teacher.profile).avatar.url
 		@profilePic = @teacher.photos.find(@teacher.profile).avatar.url
 		gon.locations = @locations
-		@photos = @teacher.photos
+		@photos = @teacher.photos.where.not(id: @teacher.profile)
 		# gon.location= [@teacher.lat, @teacher.lon]
 		# if !@teacher.locations.empty?
 		# 	gon.location = [@teacher.locations.last.latitude, @teacher.locations.last.longitude]
