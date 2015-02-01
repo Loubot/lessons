5bb4538003b12384e18848a7144e9da5a9a7294f
Louis <louisangelini@gmail.com>
2015-02-01 06:30:49 +0000

    Ahh

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -24,7 +24,7 @@ class SubjectsController < ApplicationController
 	def subject_search
 		#@subjects = Subject.all
 
-		@subjects = params[:search] == '' ? [] : Subject.where('name LIKE ?', "%#{params[:search]}%")
+		@subjects = params[:search] == '' ? [] : Subject.where('name ILIKE ?', "%#{params[:search]}%")
 	end
 
 	
