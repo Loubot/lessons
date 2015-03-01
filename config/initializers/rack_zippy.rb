63321b284e96dadc8db261e6516ad2d23485c4da
Louis Angelini <lllouis@yahoo.com>
2015-03-01 06:18:38 +0000

    gdg

--- config/initializers/rack_zippy.rb
+++ config/initializers/rack_zippy.rb
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
 # Rails.application.config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
-=======
-# Rails.application.config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
-# Rails.application.config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
-
->>>>>>> add_caching
