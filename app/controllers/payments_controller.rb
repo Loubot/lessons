b2ba3f40072f74c0b5d67d2a3d8ac08f7d5b3b53
Louis Angelini <lllouis@yahoo.com>
2015-03-11 08:50:50 +0000

    Change urls backto heroku app

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -229,8 +229,8 @@ class PaymentsController < ApplicationController
           :currency_code   => "GBP",
           :tracking_id     => params[:tracking_id],
           :cancel_url      => "https://learn-your-lesson.herokuapp.com",
-          :return_url      => "http://7ed17f44.ngrok.com/paypal-return?payKey=${payKey}",
-          :ipn_notification_url => 'http://7ed17f44.ngrok.com/store-paypal',
+          :return_url      => "https://learn-your-lesson.herokuapp.com/paypal-return?payKey=${payKey}",
+          :ipn_notification_url => 'https://learn-your-lesson.herokuapp.com/store-paypal',
           :receivers => [
             { :email => params[:teacher], amount: params[:receiver_amount], primary: true },
             { :email => 'loubotsjobs@gmail.com',  amount: 10 }
