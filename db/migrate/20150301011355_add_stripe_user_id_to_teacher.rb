4dd90876074be71aeda78666a369e7e0027c5d72
Louis <louisangelini@gmail.com>
2015-04-06 22:17:51 +0100

    add new migrations

--- db/migrate/20150301011355_add_stripe_user_id_to_teacher.rb
+++ db/migrate/20150301011355_add_stripe_user_id_to_teacher.rb
@@ -1,7 +1,11 @@
 class AddStripeUserIdToTeacher < ActiveRecord::Migration
   def change
-    add_column :teachers, :stripe_user_id, :string
-    add_column :user_carts, :multiple, :boolean, default: false
-    add_column :user_carts, :weeks, :integer, default: 0
+    add_column :teachers, 	:stripe_user_id, 	:string
+    add_column :teachers,   :address,         :string, default: ''
+    add_column :user_carts, :multiple, 				:boolean, default: false
+    add_column :user_carts, :weeks, 					:integer, default: 0
+    add_column :user_carts, :address,         :string, default: ''
+    add_column :user_carts, :home_booking, 		:boolean, defailt: false
+    add_column :prices, 		:no_map, 					:boolean, default: false
   end
 end
