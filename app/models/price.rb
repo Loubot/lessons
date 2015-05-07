14cc724d50041a413047385d95bac02a2147ddc7
Louis <louisangelini@gmail.com>
2015-05-07 16:01:25 +0100

    Change validation of no_map

--- app/models/price.rb
+++ app/models/price.rb
@@ -18,7 +18,8 @@ class Price < ActiveRecord::Base
   belongs_to :subject, touch: true
   belongs_to :teacher, touch: true
 
-  validates :subject_id, :teacher_id, :price, :no_map, presence: true
+  validates :subject_id, :teacher_id, :price, presence: true
+  validates :no_map, :inclusion => [true, false]
 
   scope :is_valid?, -> { where("home_price IS NOT NULL") }
 
