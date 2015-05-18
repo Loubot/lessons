6ef46d3b703b4e82d849e59887d906e82639f80a
Louis <louisangelini@gmail.com>
2015-05-18 21:55:41 +0100

    Fix db

--- db/migrate/20150420152010_change.rb
+++ db/migrate/20150420152010_change.rb
@@ -1,23 +1,13 @@
 class Change < ActiveRecord::Migration
   def self.up
-  	change_column :user_carts,   :home_booking,  :string, default: ''
-  	rename_column :user_carts,   :home_booking,  :booking_type
-  	add_column    :user_carts,   :package_id,	  :integer, default: 0
-    add_column    :user_carts,    :amount,        :decimal, :precision => 8, :scale => 2, default: 0.0, null: false
-    add_column    :user_carts,    :teacher_name,  :string, default: ""
-    add_column    :transactions,  :amount,        :decimal, :precision => 8, :scale => 2, default: 0.0, null: false
-    add_column    :teachers,      :paid_up,       :boolean, default: false
-    add_column    :teachers,      :paid_up_date,  :date
+    change_column :user_carts, :home_booking, :string, default: ''
+    rename_column :user_carts, :home_booking, :booking_type
+    add_column    :user_carts, :package_id,   :integer, default: 0
   end
 
   def self.down
-  	remove_column :user_carts,   :booking_type
-  	add_column :user_carts,      :home_booking, :boolean, default: false
-  	remove_column :user_carts,   :package_id
-    remove_column :user_carts,   :amount,       :decimal
-    rename_column :user_carts,   :teacher_name
-    rename_column :transactions,  :amount,      :decimal
-    remove_column :teachers,     :paid_up
-    remove_column :teachers,     :paid_up_date 
+    remove_column :user_carts, :booking_type
+    add_column :user_carts, :home_booking, :boolean, default: false
+    remove_column :user_carts, :package_id
   end
 end
