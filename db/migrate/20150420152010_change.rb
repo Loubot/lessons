f9843eb1bb1a0299f0904c76ff5a20f5d4af03d2
Louis Angelini <lllouis@yahoo.com>
2015-05-21 23:06:38 +0100

    Config for windows

--- db/migrate/20150420152010_change.rb
+++ db/migrate/20150420152010_change.rb
@@ -15,7 +15,7 @@ class Change < ActiveRecord::Migration
   	add_column :user_carts,      :home_booking, :boolean, default: false
   	remove_column :user_carts,   :package_id
     remove_column :user_carts,   :amount,       :decimal
-    rename_column :user_carts,   :teacher_name
+    rename_column :user_carts,   :teacher_name, :string
     rename_column :transactions,  :amount,      :decimal
     remove_column :teachers,     :paid_up
     remove_column :teachers,     :paid_up_date 
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150516143113) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at"
@@ -36,7 +33,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "events", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -52,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.integer  "subject_id"
   end
 
-  add_index "events", ["review_id"], name: "index_events_on_review_id", using: :btree
+  add_index "events", ["review_id"], name: "index_events_on_review_id"
 
   create_table "experiences", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -73,7 +70,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.datetime "updated_at"
   end
 
-  add_index "identities", ["teacher_id"], name: "index_identities_on_teacher_id", using: :btree
+  add_index "identities", ["teacher_id"], name: "index_identities_on_teacher_id"
 
   create_table "locations", force: :cascade do |t|
     t.integer  "teacher_id"
@@ -85,7 +82,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.datetime "updated_at"
   end
 
-  add_index "locations", ["teacher_id"], name: "index_locations_on_teacher_id", using: :btree
+  add_index "locations", ["teacher_id"], name: "index_locations_on_teacher_id"
 
   create_table "openings", force: :cascade do |t|
     t.datetime "mon_open"
@@ -114,7 +111,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.boolean  "all_day_sun",  default: false
   end
 
-  add_index "openings", ["teacher_id"], name: "index_openings_on_teacher_id", unique: true, using: :btree
+  add_index "openings", ["teacher_id"], name: "index_openings_on_teacher_id", unique: true
 
   create_table "packages", force: :cascade do |t|
     t.string   "subject_name",  default: ""
@@ -126,8 +123,8 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.datetime "updated_at",                  null: false
   end
 
-  add_index "packages", ["subject_id"], name: "index_packages_on_subject_id", using: :btree
-  add_index "packages", ["teacher_id"], name: "index_packages_on_teacher_id", using: :btree
+  add_index "packages", ["subject_id"], name: "index_packages_on_subject_id"
+  add_index "packages", ["teacher_id"], name: "index_packages_on_teacher_id"
 
   create_table "photos", force: :cascade do |t|
     t.string   "name",           limit: 255
@@ -148,8 +145,8 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.boolean  "no_map",                              default: false
   end
 
-  add_index "prices", ["subject_id"], name: "index_prices_on_subject_id", using: :btree
-  add_index "prices", ["teacher_id"], name: "index_prices_on_teacher_id", using: :btree
+  add_index "prices", ["subject_id"], name: "index_prices_on_subject_id"
+  add_index "prices", ["teacher_id"], name: "index_prices_on_teacher_id"
 
   create_table "qualifications", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -172,7 +169,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.integer  "event_id"
   end
 
-  add_index "reviews", ["event_id"], name: "index_reviews_on_event_id", using: :btree
+  add_index "reviews", ["event_id"], name: "index_reviews_on_event_id"
 
   create_table "subjects", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -186,7 +183,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.integer "teacher_id", null: false
   end
 
-  add_index "subjects_teachers", ["subject_id", "teacher_id"], name: "index_subjects_teachers_on_subject_id_and_teacher_id", using: :btree
+  add_index "subjects_teachers", ["subject_id", "teacher_id"], name: "index_subjects_teachers_on_subject_id_and_teacher_id"
 
   create_table "teachers", force: :cascade do |t|
     t.string   "first_name",             limit: 255
@@ -211,14 +208,14 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.string   "stripe_access_token",    limit: 255, default: ""
     t.boolean  "is_active",                          default: false, null: false
     t.boolean  "will_travel",                        default: false, null: false
-    t.string   "address",                            default: ""
     t.string   "stripe_user_id"
+    t.string   "address",                            default: ""
     t.boolean  "paid_up",                            default: false
     t.date     "paid_up_date"
   end
 
-  add_index "teachers", ["email"], name: "index_teachers_on_email", unique: true, using: :btree
-  add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true, using: :btree
+  add_index "teachers", ["email"], name: "index_teachers_on_email", unique: true
+  add_index "teachers", ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
 
   create_table "transactions", force: :cascade do |t|
     t.string   "sender",        limit: 255
@@ -234,7 +231,7 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.decimal  "amount",                    precision: 8, scale: 2, default: 0.0, null: false
   end
 
-  add_index "transactions", ["tracking_id"], name: "index_transactions_on_tracking_id", unique: true, using: :btree
+  add_index "transactions", ["tracking_id"], name: "index_transactions_on_tracking_id", unique: true
 
   create_table "user_carts", force: :cascade do |t|
     t.integer  "teacher_id"
@@ -247,15 +244,15 @@ ActiveRecord::Schema.define(version: 20150516143113) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "subject_id"
-    t.string   "address",                                           default: ""
-    t.integer  "weeks",                                             default: 0
     t.boolean  "multiple",                                          default: false
+    t.integer  "weeks",                                             default: 0
+    t.string   "address",                                           default: ""
     t.string   "booking_type",                                      default: ""
     t.integer  "package_id",                                        default: 0
     t.decimal  "amount",                    precision: 8, scale: 2, default: 0.0,   null: false
     t.string   "teacher_name",                                      default: ""
   end
 
-  add_index "user_carts", ["tracking_id"], name: "index_user_carts_on_tracking_id", unique: true, using: :btree
+  add_index "user_carts", ["tracking_id"], name: "index_user_carts_on_tracking_id", unique: true
 
 end
