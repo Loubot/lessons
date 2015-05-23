e2efc82af898f86e5e63c2cbf66bdba1da0e698c
Louis Angelini <lllouis@yahoo.com>
2015-05-23 07:38:32 +0100

    bla!

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,7 +6,7 @@ class InvitationsController < ApplicationController
 
     if @invitation.save
       p "url1 #{URI.escape("localhost:3000#{teach_path}?inviter_id=#{@invitation.token}")}"
-      MembershipMailer.send_invite(@invitation, ("#{teach_path}#{@invitation.token}"))
+      MembershipMailer.send_invite(current_teacher.email, @invitation, (URI.escape("localhost:3000#{teach_path}?inviter_id=#{@invitation.token}"))).deliver_now
       flash[:success] = "Invitation successfully sent"
       redirect_to URI.escape("localhost:3000#{teach_path}?inviter_id:#{@invitation.token}")
     else
