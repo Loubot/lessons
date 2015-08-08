1beff9ae560260c787bec38d180569055b791c1f
Louis <lllouis@yahoo.com>
2015-08-08 23:44:52 +0100

    more tests

--- spec/features/show_teacher_spec.rb
+++ spec/features/show_teacher_spec.rb
@@ -16,11 +16,13 @@ describe "visit show_teacher page" do
   end
 
   it "should display teachers info" do
-    expect(page).to have_content @teacher.subjects.first.name
+    expect(page).to have_content '@teacher.subjects.first.name'
     expect(page).to have_content @teacher.overview
     expect(page).to have_content @teacher.address
     expect(page).to have_content @teacher.experiences.first.title
     expect(page).to have_content @teacher.qualifications.first.title
+    expect(page).to have_css "#location0"
+    expect(page).to have_css "img[src*='fake_text']"
   end
 
  
