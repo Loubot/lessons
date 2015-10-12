6de34aaeec8a999afa9b77156ab2953aa3446b99
Louis <lllouis@yahoo.com>
2015-10-12 06:36:57 +0100

    debug

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -89,6 +89,7 @@ class StaticController < ApplicationController
 		require 'will_paginate/array' 
 		#ids = Location.near('cork', 10).select('id').map(&:teacher_id)
 		#Teacher.includes(:locations).where(id: ids)
+		logger.info "params #{ params[:search_subjects] }"
 		@subjects = Subject.where('name ILIKE ?', "%#{ params[:search_subjects] }%")
 		logger.info "first subjects #{@subjects.inspect}"
 		
